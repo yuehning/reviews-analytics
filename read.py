8ba069ec9d6b24b17ac3074f80c96a97b7f8fbf2
Yueh Ning <ning.84386@gmail.com>
2021-07-06 17:12:21 +0800

    add filtering

--- read.py
+++ read.py
@@ -13,4 +13,11 @@ for d in data: # d 表示每筆留言
 	sum_len = sum_len + len(d) 
 	# EX: 總數(10) = 總數(0)[預設0] + Len(0) [第零筆留言有10個字]
 	#     總數(30) = 總數(10) + Len(1) [第一筆留言有20個字]..............以此類推算全部留言的字數
-print('每筆留言的平均字數為', sum_len/len(data))							   
+print('每筆留言的平均字數為', sum_len/len(data))	
+
+new = [] # 新清單，裝字數小於100的留言
+for d in data:
+	if len(d) < 100: # 如果該筆留言自小於100
+		new.append(d) # 就裝入新的清單內
+print('字數小於100的留言共有', len(new), '筆')
+print(new[0])
